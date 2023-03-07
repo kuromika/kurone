00afca15c010d49d03ebca2677b000ad7c39d820
Kuromika <kuromika.dev@gmail.com>
2023-03-06 23:12:07 -0500

    Add create character POST route

--- routes/characterRoute.js
+++ routes/characterRoute.js
@@ -4,6 +4,7 @@ const characterController = require('../controllers/characterController.js');
 
 router.get("/characters", characterController.characterList);
 router.get("/character/create", characterController.getCharacterCreate);
+router.post("/character/create", characterController.postCharacterCreate);
 router.get("/character/:id", characterController.getCharacter);
 
 module.exports = router;
