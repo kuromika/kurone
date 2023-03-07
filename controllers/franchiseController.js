15a8d337a915a748ab51f4fb9d35d20a33621626
Kuromika <kuromika.dev@gmail.com>
2023-03-07 18:44:46 -0500

    Add create franchise POST controller

--- controllers/franchiseController.js
+++ controllers/franchiseController.js
@@ -1,6 +1,8 @@
 const async = require('async');
+const logController = require('./logController.js');
 const Franchise = require('../models/franchise.js');
 const Character = require('../models/character.js');
+const {body, validationResult} = require('express-validator');
 
 exports.franchiseList = (req, res, next) => {
     Franchise.find({}).sort({name: 1}).
@@ -40,3 +42,44 @@ exports.getFranchise = (req, res, next) => {
 exports.getFranchiseCreate = (req, res, next) => {
     res.render('createFranchise')
 }   
+
+exports.postFranchiseCreate = [
+    body('name', 'Name must not be empty').trim().isLength({min:1}).escape(),
+    body('description', 'Description must not be empty').trim().isLength({min:1}).escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            res.render('createFranchise', {
+                franchise: req.body,
+                errors: errors.array(),
+            })
+            return;
+        }
+        const franchise = new Franchise(
+            {
+                name: req.body.name,
+                description: req.body.description,
+            }
+        )
+        async.parallel(
+            {
+                saveFranchise(cb){
+                    franchise.save(cb);
+                },
+                saveLog(cb){
+                    logController.createLog(
+                        {
+                            type: 'Created',
+                            model: 'Franchise',
+                            references: franchise._id,
+                        }, cb)
+                }
+            }, (err, results) => {
+                if (err){
+                    return next(err);
+                }
+                res.redirect(franchise.url);
+            }
+        )
+    }
+]
