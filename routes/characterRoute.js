87802219f8cb0c0b7d84e1ea213cbd8ece2f0dea
Kuromika <kuromika.dev@gmail.com>
2023-03-06 22:23:44 -0500

    Add character create GET route

--- routes/characterRoute.js
+++ routes/characterRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const characterController = require('../controllers/characterController.js');
 
 router.get("/characters", characterController.characterList);
+router.get("/character/create", characterController.getCharacterCreate);
 router.get("/character/:id", characterController.getCharacter);
 
 module.exports = router;
