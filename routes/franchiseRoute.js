67a182c36ba54bc02c759458f459b2ac8278c670
Kuromika <kuromika.dev@gmail.com>
2023-03-07 18:44:58 -0500

    Add create franchise POST route

--- routes/franchiseRoute.js
+++ routes/franchiseRoute.js
@@ -4,6 +4,7 @@ const franchiseController = require('../controllers/franchiseController');
 
 router.get('/franchises', franchiseController.franchiseList);
 router.get('/franchise/create', franchiseController.getFranchiseCreate);
+router.post('/franchise/create', franchiseController.postFranchiseCreate);
 router.get('/franchise/:id', franchiseController.getFranchise);
 
 module.exports = router;
