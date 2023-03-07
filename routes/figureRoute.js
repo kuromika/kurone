0394d5ba3417145441dc532db4b6b3d0b21c550c
Kuromika <kuromika.dev@gmail.com>
2023-03-06 19:11:47 -0500

    Add create figure GET route

--- routes/figureRoute.js
+++ routes/figureRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const figureController = require('../controllers/figureController.js');
 
 router.get("/figures", figureController.figureList);
+router.get("/figure/create", figureController.getCreateFigure);
 router.get("/figure/:id", figureController.getFigure);
+
 
 module.exports = router;
