0ced72b8882cd7885b2e30615aad37096b3ecc5f
Kuromika <kuromika.dev@gmail.com>
2023-03-08 00:34:10 -0500

    Add delete franchise GET route

--- routes/franchiseRoute.js
+++ routes/franchiseRoute.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const franchiseController = require('../controllers/franchiseController');
 
 router.get('/franchises', franchiseController.franchiseList);
+
 router.get('/franchise/create', franchiseController.getFranchiseCreate);
 router.post('/franchise/create', franchiseController.postFranchiseCreate);
+
+router.get('/franchise/delete/:id', franchiseController.getFranchiseDelete);
+
 router.get('/franchise/:id', franchiseController.getFranchise);
 
 module.exports = router;
