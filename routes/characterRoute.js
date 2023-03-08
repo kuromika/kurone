619266c641055331dc08e9ae3b74b4c679d07b35
Kuromika <kuromika.dev@gmail.com>
2023-03-07 23:38:01 -0500

    Add character delete GET route

--- routes/characterRoute.js
+++ routes/characterRoute.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const characterController = require('../controllers/characterController.js');
 
 router.get("/characters", characterController.characterList);
+
 router.get("/character/create", characterController.getCharacterCreate);
 router.post("/character/create", characterController.postCharacterCreate);
+
+router.get('/character/delete/:id', characterController.getDeleteCharacter);
+
 router.get("/character/:id", characterController.getCharacter);
 
 module.exports = router;
