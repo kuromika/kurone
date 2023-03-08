c598a91e0ada1db4f2c16057db85b9ee3e4c8576
Kuromika <kuromika.dev@gmail.com>
2023-03-08 00:10:28 -0500

    Add character delete POST route

--- routes/characterRoute.js
+++ routes/characterRoute.js
@@ -8,6 +8,7 @@ router.get("/character/create", characterController.getCharacterCreate);
 router.post("/character/create", characterController.postCharacterCreate);
 
 router.get('/character/delete/:id', characterController.getDeleteCharacter);
+router.post('/character/delete/:id', characterController.postDeleteCharacter);
 
 router.get("/character/:id", characterController.getCharacter);
 
